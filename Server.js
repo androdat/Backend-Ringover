e6d2a20bffda168f178404fcb44fc175441054fc
androdat <saxenavedant61@gmail.com>
2022-08-13 15:46:25 +0530

    Url Updated

--- Server.js
+++ Server.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
   res.send("Use Post Method");
 });
 
-app.post("/", async (req, res) => {
+app.post("/notiontoconfluence", async (req, res) => {
   const data = req.body;
 
   const errorMsg = validateData(data);
